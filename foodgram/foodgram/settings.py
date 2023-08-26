6c85406b0328c912a208b8b03967bf4475c94bad
Яна Югай <yanayugai6@gmail.com>
2023-08-27 01:56:03 +0300

    Приведено в соответствии PEP8

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
-    'django_filters'
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +63,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-    )
+    ),
 }
 
 DJOSER = {
--- foodgram/foodgram/urls.py
+++ foodgram/foodgram/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from api.views import RecipeViewSet, TagReadView, IngredientReadView, FollowApiView
-from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from rest_framework import routers
+
+from api.views import (FollowApiView, IngredientReadView, RecipeViewSet,
+                       TagReadView)
 
 router = routers.DefaultRouter()
 router.register(r'recipes', RecipeViewSet)
@@ -12,7 +14,10 @@ router.register(r'ingredients', IngredientReadView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/<int:user_id>/subscribe/', FollowApiView.as_view({'post': 'create', 'delete': 'destroy'})),
+    path(
+        'api/users/<int:user_id>/subscribe/',
+        FollowApiView.as_view({'post': 'create', 'delete': 'destroy'}),
+    ),
     path('api/users/subscriptions/', FollowApiView.as_view({'get': 'list'})),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),
@@ -20,5 +25,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,
+    )
