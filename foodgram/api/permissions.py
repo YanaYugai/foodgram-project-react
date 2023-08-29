6e15fcb2f8c2b45c33ef43b0f80a7e6d4e8456a4
Яна Югай <yanayugai6@gmail.com>
2023-08-29 22:41:45 +0300

    First review

--- foodgram/api/permissions.py
+++ foodgram/api/permissions.py
@@ -7,18 +7,6 @@ from recipe.models import Recipe
 class IsUserAdminAuthorOrReadOnly(BasePermission):
     """Разрешение редактирования только Админу и Автору."""
 
-    def has_permission(self, request: Request, _) -> bool:
-        """Проверка прав пользователя на доступ к запросу.
-
-        Args:
-            request: Запрос пользователя.
-
-        Returns:
-            True - функция сработала удачно, иначе False.
-
-        """
-        return request.method in SAFE_METHODS or request.user.is_authenticated
-
     def has_object_permission(self, request: Request, _, obj: Recipe) -> bool:
         """Проверка прав пользователя на доступ к объекту.
 
