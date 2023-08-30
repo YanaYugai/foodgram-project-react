8e99ad5e175312e39809e566626b9db204c55230
Яна Югай <yanayugai6@gmail.com>
2023-08-30 14:10:35 +0300

    Первое ревью

--- foodgram/api/management/commands/comand.py
+++ foodgram/api/management/commands/comand.py
@@ -1,8 +1,8 @@
+import csv
 import os
-from csv import DictReader
 
 from django.conf import settings
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 from recipe.models import Ingredient, Tag
 
@@ -16,30 +16,34 @@ class Command(BaseCommand):
     """Загрузчик данных с csv-файла."""
 
     def handle(self, *args, **options):
-        for csv, model in MODEL_CSV.items():
+        self.stdout.write(
+            self.style.HTTP_INFO('Загрузка в базы данных началась'),
+        )
+        for file, model in MODEL_CSV.items():
             csv_file = os.path.join(
                 os.path.join(settings.BASE_DIR, 'data'),
-                csv,
+                file,
             )
-            if model == Ingredient:
+            try:
                 with open(csv_file, 'r', encoding='utf-8') as f:
-                    fieldnames = ['name', 'measurement_unit']
-                    reader = DictReader(f, fieldnames=fieldnames)
-                    for row in reader:
-                        model.objects.get_or_create(
-                            name=row['name'],
-                            measurement_unit=row['measurement_unit'],
-                        )
-            else:
-                with open(csv_file, 'r', encoding='utf-8') as f:
-                    fieldnames = ['name', 'color', 'slug']
-                    reader = DictReader(f, fieldnames=fieldnames)
-                    for row in reader:
-                        model.objects.get_or_create(
-                            name=row['name'],
-                            color=row['color'],
-                            slug=row['slug'],
-                        )
+                    reader = csv.reader(f)
+                    if model == Ingredient:
+                        for row in reader:
+                            name, measurement_unit = row
+                            model.objects.get_or_create(
+                                name=name,
+                                measurement_unit=measurement_unit,
+                            )
+                    else:
+                        for row in reader:
+                            name, color, slug = row
+                            model.objects.get_or_create(
+                                name=name,
+                                color=color,
+                                slug=slug,
+                            )
+            except FileNotFoundError:
+                raise CommandError('Не найден файл с данными!')
         self.stdout.write(
             self.style.SUCCESS('Данные из .csv ' 'файлов загружены успешно!'),
         )
